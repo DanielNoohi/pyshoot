7c0864c43a026be6a6b81b2bf5aa3120d00c5090
Mohammad Ali Sadigh Noohi <37836576+DanielNoohi@users.noreply.github.com>
2023-10-05 00:58:42 +0330

    Create pinstall.sh
    main code

---
+++ pinstall.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+echo "Update Everything..."
+
+sleep 0.5
+
+echo 
+yellow_msg 'Updating the System.'
+echo 
+
+sleep 0.5
+
+sleep 0.5
+
+sudo apt update
+sudo apt -y upgrade
+sudo apt -y dist-upgrade
+sudo apt -y autoremove
+sleep 0.5
+    
+# Again :D
+sudo apt -y autoclean
+sudo apt -y clean
+sudo apt update
+sudo apt -y upgrade
+sudo apt -y dist-upgrade
+sudo apt -y autoremove
+
+echo "Installing essential Python packages and tools..."
+
+# Check if Python is installed
+if command -v python3 &>/dev/null; then
+    echo "Python is already installed. Proceeding with package installation."
+else
+    echo "Python is not installed. Please install Python and run this script again."
+    exit 1
+fi
+
+# Install pip (if not installed)
+if command -v pip &>/dev/null; then
+    echo "pip is already installed."
+else
+    echo "Installing pip..."
+    sudo apt-get update
+    sudo apt-get install python3-pip
+fi
+
+# Install common Python packages
+pip install virtualenv django numpy requests
+
+# Install Telegram bot library
+pip install python-telegram-bot
+
+echo "Installation complete. Enjoy coding with Django and other essential Python packages!"
